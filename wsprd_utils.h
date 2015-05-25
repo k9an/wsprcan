e15410bd4412c1cbe6cabf67a2e9f0ff139c3136
k9an <s.j.franke@icloud.com>
2015-05-24 20:09:51 -0500

    Added sanity checks on power/grid. Moved callsign unpacking to a function.

--- wsprd_utils.h
+++ wsprd_utils.h
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+#include <stdint.h>
+#include <time.h>
 
 void unpack50( signed char *dat, int32_t *n1, int32_t *n2 );
 
@@ -15,3 +19,6 @@ void deinterleave(unsigned char *sym);
 // used by qsort
 int floatcomp(const void* elem1, const void* elem2);
 
+unsigned int nhash_( const void *key, size_t length, uint32_t initval);
+
+int unpk_( signed char *message, char hashtab[32768][13], char *call_loc_pow, char *callsign);
